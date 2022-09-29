4d7f0e3e3ed6d16ae2d86831d97d85f8153e2add
Jesus Alberto <jesyrom0825@gmail.com>
2022-09-29 00:06:18 -0500

    corriengiendo 12

--- pages/listaInvitados.js
+++ pages/listaInvitados.js
@@ -69,10 +69,28 @@ const ListaInvitados = ({ titulo }) => {
 
     }
 
+    const agregarInvitado = async(e)=>{
+        e.preventDefault();
+        const nuevoInvitado = {
+            nombre: e.target.name
+        }
+        const result = await axios.post(`http://www.goweddings.net/admin/lista-invitados/agregarNuevo` );
+        
+    }
+
 
     return (
         <>
-            <Header titulo={'Lista de Invitados Completa'}/>
+            <Header className='titulo' titulo={'Lista de Invitados'}/>
+            <div className='containerInputs'>
+                <form action="#" method="post" onSubmit={agregarInvitado}>
+                    <input className='camposForm' type="text" name="nombre" id="" placeholder='Invitado' />
+                    <input className='camposForm' type="text" name="grupo" id="" placeholder='Grupo' />
+                    <input className='camposForm' type="text" name="mesa" id="" placeholder='Mesa' />
+                    <button type='submit' className='botonAgregar' >Agregar Invitado</button>
+                </form>
+                
+            </div>
             <div className='listaInvitados'> 
                 {
                     invitados.map(( invitado ) => {
